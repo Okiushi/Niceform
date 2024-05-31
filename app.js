1e4b323ac2f9f1e0b5cf3b680617b3c4d0fee59d
Okiushi <loic.debuchy@gmail.com>
2024-05-31 12:27:40 +0200

    Squashed commit of the following:
    commit 4f138cdc99bc0cc335380be8e554821f2df03215
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Fri May 31 12:22:34 2024 +0200
    
        Revert "Squashed commit of the following:"
    
        This reverts commit fa717d9e70f819f5366215ae90be32ba768c6d63.
    
    commit fa717d9e70f819f5366215ae90be32ba768c6d63
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Fri May 31 12:21:56 2024 +0200
    
        Squashed commit of the following:
    
        commit d541c206b086c3ef4a5a9f893a793443f6076005
        Author: Okiushi <loic.debuchy@gmail.com>
        Date:   Tue Apr 16 19:09:05 2024 +0200
    
            Create test_create.js for save crud test
    
            - Create test_create.js
            - Create crud_module.js
    
        commit 674fac5ffa6334d9d15a1296508719e5114bb30e
        Author: Okiushi <100536875+Okiushi@users.noreply.github.com>
        Date:   Mon Apr 8 12:48:01 2024 +0200
    
            Adding mongodb package
    
        commit 71e510b3ddd4e9763481e7463e4ec0cb5dfdb5db
        Author: Okiushi <100536875+Okiushi@users.noreply.github.com>
        Date:   Mon Apr 8 12:39:16 2024 +0200
    
            Using an .env file
    
            - The PORT variable has been renamed SRV_PORT (Several ports in variable environment are to come)
            - dotenv package added
    
    commit 8dc466ecc93b702b582482beec2e117f7a741d5b
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Fri May 31 12:16:36 2024 +0200
    
        Finalizing small changes
    
    commit 74d8bed9c514bd6b42da3cc0e084ffe39d626d56
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 21:18:42 2024 +0200
    
        deployement test
    
    commit f08e1428c0eaf58fd24226bb3a7080b7054d3580
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 19:42:48 2024 +0200
    
        Fixed port problem
    
    commit d0bd005d646fd2d300deb5783d9eca8cd9db9bba
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 19:32:46 2024 +0200
    
        Little fix
    
    commit 6cf1ced2627c7287f78769ff98280c8b59985b58
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 19:21:08 2024 +0200
    
        fix for run command
    
    commit f899f53135d205f9300cf55f4e1126088ae314bb
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 19:06:38 2024 +0200
    
        tiny fix too
    
    commit 9004acebfc9af5321069a8bc4f006287646ec366
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 18:59:26 2024 +0200
    
        less log
    
    commit 454d9c948b219b2f536f8621316e1ea764e3df3f
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 18:57:28 2024 +0200
    
        Tiny fix
    
    commit 9d49b2afb5e9b685283f73782f4a93711270ddf0
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 18:13:43 2024 +0200
    
        Better arrivale on VPS
    
        - Add node dependance on the VPS
        - Not edit the .env file on VPS
        - Autostart app.js
    
    commit c85f3e6e0751a65d569fded5d702684d5b3f13e0
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 16:57:56 2024 +0200
    
        Another alternative too
    
    commit 23958e565b834b982220a19713c4a681ab1d437e
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 15:31:02 2024 +0200
    
        Deleting a step
    
    commit b7ac6dae25bf9dc8916f1eb530004185234250c1
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 15:26:50 2024 +0200
    
        Another aternative for deploy
    
    commit 0cdb072e4fdbe507f575fb052d66a10b19ed7055
    Author: Okiushi <loic.debuchy@gmail.com>
    Date:   Thu May 30 14:34:11 2024 +0200
    
        Test an alternative for deployement
    
        - deploy.yml edited
        - .env added to gitignore

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const app = express()
+require('dotenv').config();
 
 // Définition du port d'écoute sur 3000 par défaut si non défini dans les variables d'environnement
-const port= process.env.PORT || 3000;
+const port= process.env.SRV_PORT || 3000;
+const defaultRoute = process.env.DEFAULT_ROUTE || '/';
 
 // Import des routes et association à l'application
 const indexRouter = require('./routes/index_router');
 const devRouter = require('./routes/devhome_router');
 
 // Association des routes à l'application
-app.use('/', indexRouter);
-app.use('/dev', devRouter);
+app.use(defaultRoute + '/', indexRouter);
+app.use(defaultRoute + '/dev', devRouter);
 
 app.set('view engine', 'ejs'); // Définition du moteur de rendu de vue
 
 // Lancement de l'application
 app.listen(port, () => {
-    console.log(`Starting the application listening on port ${port}`)
-})
+    console.log(`Niceforms application launched on http://localhost:${port}${defaultRoute}`);
+});
